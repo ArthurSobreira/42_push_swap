8af7f45de1612342d1dec199f2d0ac601b11a517
Arthur Sobreira <tutusobreira@gmail.com>
2023-11-20 18:08:30 -0300

    Update error.c

--- src/mandatory/error.c
+++ src/mandatory/error.c
@@ -6,8 +6,15 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 16:37:48 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/11/16 16:38:16 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/11/20 17:11:48 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	handle_error(short exit_code)
+{
+	if (exit_code == 1)
+		ft_putstr_fd("Error\n", STDERR_FILENO);
+	exit(EXIT_FAILURE);
+}
