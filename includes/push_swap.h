54f2ee923d5af7cb26a1a8a8710929dd5d310211
Arthur Sobreira <tutusobreira@gmail.com>
2023-11-20 18:08:08 -0300

    Update push_swap header

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 16:41:22 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/11/15 16:45:33 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/11/20 17:38:10 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,22 @@
 # define PUSH_SWAP_H
 
 # include "../libs/libft/libft.h"
+# include <unistd.h>
+# include <stdlib.h>
 
+typedef enum e_bool
+{
+	FALSE,
+	TRUE
+}	t_bool;
+
+typedef struct s_stack
+{
+	int				value;
+	struct s_stack	*prev;
+	struct s_stack	*next;
+}					t_stack;
+
+void	handle_error(short exit_code);
 
 #endif
