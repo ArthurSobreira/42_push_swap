1c8c859862295d02b8bdd5e659259c6b0f20ea85
Arthur Sobreira <tutusobreira@gmail.com>
2023-11-21 11:22:31 -0300

    Update push_swap header

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 16:41:22 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/11/21 10:47:55 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/11/21 11:22:10 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,9 @@
 
 # define INT_MAX 2147483647
 # define INT_MIN -2147483648
+# define EXIT_ERROR -1
+# define EXIT_SUCCESS 0
+# define EXIT_FAILURE 1
 
 typedef enum e_bool
 {
@@ -33,6 +36,16 @@ typedef struct s_stack
 	struct s_stack	*next;
 }					t_stack;
 
+typedef struct s_push
+{
+	char	**argv;
+	t_stack	*a;
+	t_stack	*b;
+}				t_push;
+
+void	clear_all(t_push *push_swap, short exit_code);
 void	handle_error(short exit_code);
+void	free_split(char **split);
+t_push	*init_push_swap(void);
 
 #endif
