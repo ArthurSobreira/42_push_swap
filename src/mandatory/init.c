da6c63a9fa549ee3c423d6734be90713692fb2f7
Arthur Sobreira <tutusobreira@gmail.com>
2023-11-21 11:21:49 -0300

    Add init.c

---
+++ src/mandatory/init.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/21 11:19:38 by arsobrei          #+#    #+#             */
+/*   Updated: 2023/11/21 11:19:59 by arsobrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+t_push	*init_push_swap(void)
+{
+	t_push	*ps;
+
+	ps = (t_push *)malloc(sizeof(t_push));
+	if (!ps)
+		handle_error(-1);
+	ps->argv = NULL;
+	ps->a = NULL;
+	ps->b = NULL;
+	return (ps);
+}
