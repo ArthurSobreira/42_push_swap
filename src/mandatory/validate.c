5df297f8c0d4398b5d1acf9a7853f122ec28582a
Arthur Sobreira <tutusobreira@gmail.com>
2023-11-21 17:10:46 -0300

    Update validate.c

--- src/mandatory/validate.c
+++ src/mandatory/validate.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 16:15:04 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/11/21 16:15:44 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/11/21 17:08:31 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,14 @@ void	validate_args(t_push *push_swap)
 	size_t	index;
 	long	number;
 
-	if (*push_swap->argv == NULL)
+	if (!check_duplication(push_swap->argv) || *push_swap->argv == NULL)
 		clear_all(push_swap, EXIT_FAILURE);
 	index = 0;
 	number = 0;
 	while (push_swap->argv[index])
 	{
 		// ft_printf("argv[%d] = %s\n", index, push_swap->argv[index]);
-		if (!valid_sintax(push_swap->argv[index]))
+		if (!check_sintax(push_swap->argv[index]))
 			clear_all(push_swap, EXIT_FAILURE);
 		number = ft_atol(push_swap->argv[index]);
 		if (number > INT_MAX || number < INT_MIN)
@@ -33,7 +33,29 @@ void	validate_args(t_push *push_swap)
 	}
 }
 
-t_bool	valid_sintax(char *number)
+t_bool	check_duplication(char *args_list[])
+{
+	size_t	index;
+	size_t	index2;
+	long	number;
+
+	index = 0;
+	while (args_list[index])
+	{
+		number = ft_atol(args_list[index]);
+		index2 = index + 1;
+		while (args_list[index2])
+		{
+			if (number == ft_atol(args_list[index2]))
+				return (FALSE);
+			index2++;
+		}
+		index++;
+	}
+	return (TRUE);
+}
+
+t_bool	check_sintax(char *number)
 {
 	size_t	index;
 	t_bool	valid;
