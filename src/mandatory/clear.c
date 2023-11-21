0a12535f945c48b164a81f2ebd0ed126749ebbab
Arthur Sobreira <tutusobreira@gmail.com>
2023-11-21 17:07:34 -0300

    Update clear.c

--- src/mandatory/clear.c
+++ src/mandatory/clear.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 11:15:20 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/11/21 11:45:15 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/11/21 17:06:21 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,6 @@
 
 void	clear_all(t_push *push_swap, short exit_code)
 {
-	// if (push_swap->a)
-	// 	clear_stack(push_swap->a);
-	// if (push_swap->b)
-	// 	clear_stack(push_swap->b);
 	if (push_swap->argv && push_swap->checker == TRUE)
 		free_split(push_swap->argv);
 	free(push_swap);
