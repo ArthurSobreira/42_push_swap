cee888eabfff9ec8b5dcf8c012033300774551c7
Arthur Sobreira <tutusobreira@gmail.com>
2023-11-22 15:43:15 -0300

    Update push_swap header

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 16:41:22 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/11/22 14:52:39 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/11/22 15:40:07 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,13 +38,18 @@ typedef struct s_stack
 
 typedef struct s_push
 {
-	t_stack	*a;
-	t_stack	*b;
+	t_stack	*stack_a;
+	t_stack	*stack_b;
 	t_bool	split_checker;
 	char	**argv;
 }				t_push;
 
 t_push	*init_push_swap(int argc, char *argv[]);
+void	init_stack(t_push *push_swap);
+
+void	link_node(t_stack **stack, int number);
+t_stack	*create_node(int value);
+t_stack	*find_last_node(t_stack *stack);
 
 void	validate_args(t_push *push_swap);
 t_bool	check_sintax(char *number);
