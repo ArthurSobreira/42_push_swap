ecded3f98c73b5fff2d243872d598870f344c4f4
Arthur Sobreira <tutusobreira@gmail.com>
2023-11-23 11:23:52 -0300

    Update main.c

--- src/mandatory/main.c
+++ src/mandatory/main.c
@@ -6,20 +6,11 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 15:35:57 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/11/22 20:13:51 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/11/23 11:23:01 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-void	print_elements(t_stack *stack)
-{
-	while (stack)
-	{
-		ft_printf("%d\n", stack->value);
-		stack = stack->next;
-	}
-}
 
 int	main(int argc, char *argv[])
 {
@@ -37,7 +28,6 @@ int	main(int argc, char *argv[])
 			if (push_swap->length_a <= 5)
 				small_sort(push_swap);
 		}
-		// print_elements(push_swap->stack_a);
 		clear_all(push_swap, EXIT_SUCCESS);
 	}
 	return (0);
