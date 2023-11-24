4cca9d74e7d381efdd06f48c5b015317b754ee32
Arthur Sobreira <tutusobreira@gmail.com>
2023-11-24 16:48:59 -0300

    Update cmd_push.c

--- src/mandatory/cmd_push.c
+++ src/mandatory/cmd_push.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 12:02:18 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/11/23 12:15:44 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/11/24 16:47:59 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,26 @@ void	push(t_push *push_swap, t_cmd type, t_bool print_checker)
 	}
 }
 
-// void	execute_push(t_stack **stack_1, t_stack **stack_2)
-// {
+void	execute_push(t_stack **dst_stack, t_stack **src_stack)
+{
+	t_stack	*push_node;
 
-// }
+	if (*src_stack == NULL)
+		handle_error(EXIT_FAILURE);
+	push_node = *src_stack;
+	*src_stack = (*src_stack)->next;
+	if (*src_stack != NULL)
+		(*src_stack)->prev = NULL;
+	push_node->prev = NULL;
+	if (*dst_stack == NULL)
+	{
+		*dst_stack = push_node;
+		push_node->next = NULL;
+	}
+	else
+	{
+		push_node->next = *dst_stack;
+		(*dst_stack)->prev = push_node;
+		*dst_stack = push_node;
+	}
+}
