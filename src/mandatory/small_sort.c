5a47a6767b3b55dcbf8f99b384e064ecfc755912
Arthur Sobreira <tutusobreira@gmail.com>
2023-11-28 19:53:01 -0300

    Update small_sort.c

--- src/mandatory/small_sort.c
+++ src/mandatory/small_sort.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 18:03:11 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/11/28 10:52:55 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/11/28 19:46:44 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,18 @@ void	small_sort(t_push *push_swap)
 		swap(push_swap, sa, TRUE);
 	else if (push_swap->length_a == 3)
 		sort_three(push_swap);
+	else
+		sort_few_elements(push_swap);
 }
 
 void	sort_three(t_push *push_swap)
 {
-	t_stack	*stack_a;
 	short	lowest_pos;
 	short	highest_pos;
 
-	stack_a = push_swap->stack_a;
-	lowest_pos = find_low_position(stack_a);
-	highest_pos = find_high_position(stack_a);
+	set_position(push_swap->stack_a);
+	lowest_pos = find_low_position(push_swap->stack_a);
+	highest_pos = find_high_position(push_swap->stack_a);
 	if (lowest_pos == 0)
 	{
 		rev_rotate(push_swap, rra, TRUE);
@@ -47,4 +48,32 @@ void	sort_three(t_push *push_swap)
 			swap(push_swap, sa, TRUE);
 		rev_rotate(push_swap, rra, TRUE);
 	}
+}
+
+void	sort_few_elements(t_push *push_swap)
+{
+	short	lowest_pos;
+	int		center;
+
+	while (stack_length(push_swap->stack_a) != 3)
+	{
+		set_position(push_swap->stack_a);
+		lowest_pos = find_low_position(push_swap->stack_a);
+		center = push_swap->length_a / 2;
+		if (lowest_pos <= center)
+		{
+			while (push_swap->stack_a->position != lowest_pos)
+				rotate(push_swap, ra, TRUE);
+		}
+		else if (lowest_pos > center)
+		{
+			while (push_swap->stack_a->position != lowest_pos)
+				rev_rotate(push_swap, rra, TRUE);
+		}
+		push(push_swap, pb, TRUE);
+	}
+	if (!is_ordered(push_swap->stack_a))
+		sort_three(push_swap);
+	while (push_swap->stack_b != NULL)
+		push(push_swap, pa, TRUE);
 }
