8ff096ebfab8ef810ff15c6f5afce8e5f5dbd871
Arthur Sobreira <tutusobreira@gmail.com>
2023-11-30 12:41:47 -0300

    Update small_sort.c

--- src/mandatory/small_sort.c
+++ src/mandatory/small_sort.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 18:03:11 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/11/30 12:22:05 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/11/30 12:37:37 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,27 +24,27 @@ void	small_sort(t_push *push_swap)
 
 void	sort_three(t_push *push_swap)
 {
-	short	smaller_pos;
-	short	bigger_pos;
+	short	smallest_pos;
+	short	biggest_pos;
 
 	set_position(push_swap->stack_a);
-	smaller_pos = find_smaller_position(push_swap->stack_a);
-	bigger_pos = find_bigger_position(push_swap->stack_a);
-	if (smaller_pos == 0)
+	smallest_pos = find_smallest_position(push_swap->stack_a);
+	biggest_pos = find_biggest_position(push_swap->stack_a);
+	if (smallest_pos == 0)
 	{
 		rev_rotate(push_swap, rra, TRUE);
 		swap(push_swap, sa, TRUE);
 	}
-	else if (smaller_pos == 1)
+	else if (smallest_pos == 1)
 	{
-		if (bigger_pos == 0)
+		if (biggest_pos == 0)
 			rotate(push_swap, ra, TRUE);
 		else
 			swap(push_swap, sa, TRUE);
 	}
-	else if (smaller_pos == 2)
+	else if (smallest_pos == 2)
 	{
-		if (bigger_pos == 0)
+		if (biggest_pos == 0)
 			swap(push_swap, sa, TRUE);
 		rev_rotate(push_swap, rra, TRUE);
 	}
@@ -52,22 +52,22 @@ void	sort_three(t_push *push_swap)
 
 void	sort_few_elements(t_push *push_swap)
 {
-	short	smaller_pos;
-	t_stack	*smaller_node;
+	short	smallest_pos;
+	t_stack	*smallest_node;
 
 	while (stack_length(push_swap->stack_a) != 3)
 	{
 		set_position(push_swap->stack_a);
-		smaller_pos = find_smaller_position(push_swap->stack_a);
-		smaller_node = get_node(push_swap->stack_a, smaller_pos);
-		if (smaller_node->above_median)
+		smallest_pos = find_smallest_position(push_swap->stack_a);
+		smallest_node = get_node(push_swap->stack_a, smallest_pos);
+		if (smallest_node->above_median)
 		{
-			while (push_swap->stack_a->position != smaller_pos)
+			while (push_swap->stack_a->position != smallest_pos)
 				rotate(push_swap, ra, TRUE);
 		}
-		else if (!smaller_node->above_median)
+		else if (!smallest_node->above_median)
 		{
-			while (push_swap->stack_a->position != smaller_pos)
+			while (push_swap->stack_a->position != smallest_pos)
 				rev_rotate(push_swap, rra, TRUE);
 		}
 		push(push_swap, pb, TRUE);
