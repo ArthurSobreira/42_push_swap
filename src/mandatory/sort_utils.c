41908aab546e8a89dce4da6e8ec046a0c2ccc26d
Arthur Sobreira <tutusobreira@gmail.com>
2023-11-30 11:54:07 -0300

    Update sort_utils.c

--- src/mandatory/sort_utils.c
+++ src/mandatory/sort_utils.c
@@ -6,11 +6,32 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 10:47:22 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/11/28 13:16:03 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/11/30 11:06:34 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	set_position(t_stack *stack)
+{
+	int	current_pos;
+	int	center_line;
+
+	if (stack == NULL)
+		return ;
+	current_pos = 0;
+	center_line = stack_length(stack) / 2;
+	while (stack != NULL)
+	{
+		stack->position = current_pos;
+		if (current_pos <= center_line)
+			stack->above_median = TRUE;
+		else if (current_pos > center_line)
+			stack->above_median = FALSE;
+		stack = stack->next;
+		++current_pos;
+	}
+}
 
 short	find_high_position(t_stack *stack)
 {
