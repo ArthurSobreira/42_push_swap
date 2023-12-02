101eaf9b51f4c9dc772a15a6bb07a41963d63f0c
Arthur <tutusobreira@gmail.com>
2023-12-02 18:39:36 -0300

    Update clear_bonus.c

--- src/bonus/clear_bonus.c
+++ src/bonus/clear_bonus.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 11:15:20 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/12/02 16:23:34 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/12/02 18:35:52 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,14 @@ void	clear_all(t_push *push_swap, short exit_code)
 	if (push_swap->stack_a)
 		free_stack(&push_swap->stack_a);
 	free(push_swap);
+	get_next_line(CLEAR_STATIC);
 	handle_error(exit_code);
+}
+
+void	clear_invalid_command(t_push *push_swap, char *command)
+{
+	free(command);
+	clear_all(push_swap, EXIT_FAILURE);
 }
 
 void	free_split(char **split)
