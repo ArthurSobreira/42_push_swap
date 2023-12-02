afde27d28c72876a7dc4692961c35effdd41441d
Arthur <tutusobreira@gmail.com>
2023-12-02 17:44:42 -0300

    Update cmd_rev_rotate.c

--- src/mandatory/cmd_rev_rotate.c
+++ src/mandatory/cmd_rev_rotate.c
@@ -6,36 +6,36 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 11:32:34 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/11/28 15:53:27 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/12/02 17:41:51 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	rev_rotate(t_push *push_swap, t_cmd type, t_bool print_checker)
+void	reverse_rotate(t_push *push_swap, t_cmd type, t_bool print_checker)
 {
 	if (type == rra)
 	{
-		execute_rev_rotate(&push_swap->stack_a);
+		execute_reverse_rotate(&push_swap->stack_a);
 		if (print_checker)
 			ft_printf("rra\n");
 	}
 	else if (type == rrb)
 	{
-		execute_rev_rotate(&push_swap->stack_b);
+		execute_reverse_rotate(&push_swap->stack_b);
 		if (print_checker)
 			ft_printf("rrb\n");
 	}
 	else if (type == rrr)
 	{
-		execute_rev_rotate(&push_swap->stack_a);
-		execute_rev_rotate(&push_swap->stack_b);
+		execute_reverse_rotate(&push_swap->stack_a);
+		execute_reverse_rotate(&push_swap->stack_b);
 		if (print_checker)
 			ft_printf("rrr\n");
 	}
 }
 
-void	execute_rev_rotate(t_stack **stack)
+void	execute_reverse_rotate(t_stack **stack)
 {
 	t_stack	*current_first;
 	t_stack	*new_first;
