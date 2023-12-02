fe6b9368f0deb570af1411ae27c25a088ec551bc
Arthur <tutusobreira@gmail.com>
2023-12-02 17:50:52 -0300

    Update stack_utils_bonus.c

---
+++ src/bonus/stack_utils_bonus.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_utils_bonus.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/23 12:20:53 by arsobrei          #+#    #+#             */
+/*   Updated: 2023/12/02 16:25:47 by arsobrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "checker.h"
+
+t_bool	is_ordered(t_stack *stack)
+{
+	t_stack	*temp;
+
+	if (stack == NULL)
+		handle_error(EXIT_FAILURE);
+	temp = stack;
+	while (temp->next)
+	{
+		if (temp->value > temp->next->value)
+			return (FALSE);
+		temp = temp->next;
+	}
+	return (TRUE);
+}
+
+size_t	stack_length(t_stack *stack)
+{
+	size_t	length;
+
+	if (stack == NULL)
+		handle_error(EXIT_FAILURE);
+	length = 1;
+	while (stack->next != NULL)
+	{
+		length++;
+		stack = stack->next;
+	}
+	return (length);
+}
+
+void	free_stack(t_stack **stack)
+{
+	t_stack	*temp;
+
+	if (stack != NULL)
+	{
+		while (*stack)
+		{
+			temp = *stack;
+			*stack = (*stack)->next;
+			free(temp);
+		}
+		*stack = NULL;
+	}
+}
