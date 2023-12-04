c35631be18d9b6948bf827aba99b59a21aeb4637
Arthur Sobreira <tutusobreira@gmail.com>
2023-12-04 11:06:44 -0300

    Update read_commands.c

--- src/bonus/read_commands.c
+++ src/bonus/read_commands.c
@@ -2,9 +2,12 @@
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   read_commands.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
+/*                                                    +:+ +:+        
+	+:+     */
+/*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+      
+	+#+        */
+/*                                                +#+#+#+#+#+  
+	+#+           */
 /*   Created: 2023/12/02 18:12:01 by arsobrei          #+#    #+#             */
 /*   Updated: 2023/12/02 18:12:01 by arsobrei         ###   ########.fr       */
 /*                                                                            */
@@ -17,11 +20,12 @@ void	read_commands(t_push *push_swap)
 	char	*command;
 	int		counter;
 	size_t	len;
-	
+
 	counter = 1;
 	while (TRUE)
 	{
-		ft_printf("\033[35;3m%dst command\033[0m> ", counter++);
+		ft_printf("\033[36;3m%dst command\033[0m\033[37;3m>\033[0m ",
+			counter++);
 		command = get_next_line(STDIN_FILENO);
 		if (command == NULL)
 			break ;
@@ -29,7 +33,7 @@ void	read_commands(t_push *push_swap)
 		free(command);
 	}
 	len = stack_length(push_swap->stack_a);
-	if ((is_ordered(push_swap->stack_a)) && (len == push_swap->length_a) \
+	if ((is_ordered(push_swap->stack_a)) && (len == push_swap->length_a)
 		&& (push_swap->stack_b == NULL))
 		ft_printf("\n\033[32;1mOK\033[0m\n");
 	else
