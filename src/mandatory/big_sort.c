9baa28a797e019779207cdd337b31b8b5b7e1997
Arthur Sobreira <tutusobreira@gmail.com>
2023-12-04 20:10:09 -0300

    Update big_sort.c

--- src/mandatory/big_sort.c
+++ src/mandatory/big_sort.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 18:17:09 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/12/04 18:20:48 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/12/04 19:59:54 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ void	big_sort(t_push *push_swap)
 		set_position(push_swap->stack_b);
 		set_match_node(push_swap->stack_a, push_swap->stack_b);
 		set_price(push_swap->stack_a, push_swap->stack_b);
+		set_cheapest(push_swap->stack_b);
 		push_swap->stack_b = push_swap->stack_b->next;
 	}
 	smallest_pos = find_smallest_position(push_swap->stack_a);
