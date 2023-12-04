4d5990167d0bf2fb7ce5e357fa9f0d00800bfeee
Arthur Sobreira <tutusobreira@gmail.com>
2023-12-04 11:07:05 -0300

    Update main_bonus.c

--- src/bonus/main_bonus.c
+++ src/bonus/main_bonus.c
@@ -6,32 +6,11 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 15:35:57 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/12/02 18:12:35 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/12/04 10:47:05 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "checker.h"
-#include <stdio.h> // Delete later
-
-void	print_elements(t_stack *stack, char title)
-{
-	printf("                        STACK %c                       \n", title);
-	printf("|-----------------|-----------------|-----------------|\n");
-	printf("| %-15s | %-15s | %-15s |\n", "prev->value", "value",
-		"next->value");
-	printf("|-----------------|-----------------|-----------------|\n");
-	while (stack)
-	{
-		printf("| %-15d | %-15d | %-15d |\n",
-			(stack->prev != NULL) ? stack->prev->value : 0x0,
-			stack->value,
-			(stack->next != NULL) ? stack->next->value : 0x0);
-		stack = stack->next;
-		if (stack != NULL)
-			printf("|-----------------|-----------------|-----------------|\n");
-	}
-	printf("\n");
-}
 
 int	main(int argc, char *argv[])
 {
